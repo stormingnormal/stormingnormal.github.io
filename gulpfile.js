d975f0079179e478afae322f7c674066d3bf297d
David Nash <david.nash@interdirect.co.uk>
2019-07-09 14:43:30 +0100

    favicons

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ const del                   = require('del');
 const concat                = require('gulp-concat');
 const gp_uglify             = require('gulp-uglify');
 const gp_uglify_css             = require('gulp-uglifycss');
-
+const favicons = require('gulp-favicons');
 
 
 function styles() {
@@ -35,11 +35,35 @@ function scripts(){
         './bower_components/jquery/dist/jquery.js',
         './bower_components/foundation-sites/dist/js/foundation.js'
     ])
-    //.pipe(gp_uglify())
+    .pipe(gp_uglify())
     .pipe(concat('app.js'))
     .pipe(gulp.dest('./static/js'));
 } 
         
+function favicon(){
+    return gulp.src('./src/images/favicon.png')
+            .pipe(
+                favicons({
+                appName: 'Eskimo-Nation',
+                appShortName: 'Eskimo-Nation',
+                appDescription: 'Eskimo-Nation',
+                developerName: 'David Nash',
+                developerURL: 'https://www.eskimo-nation.co.uk/',
+                background: '#020307',
+                path: 'static/',
+                lang: "en-GB",
+                url: 'https://www.eskimo-nation.co.uk/',
+                display: 'standalone',
+                orientation: 'portrait',
+                scope: '/',
+                start_url: '/',
+                version: 1.0,
+                logging: false,
+                html: 'favicons.html',
+                pipeHTML: true,
+                replace: true,
+                }))
+        .pipe(gulp.dest('./static/'));
+}
 
-
-gulp.task('default',gulp.series(scripts,watchFiles)); 
+gulp.task('default',gulp.series(favicon,scripts,watchFiles)); 
